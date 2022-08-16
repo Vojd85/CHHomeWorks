b531be7038b38b70c2420f36cc1f460bfca02605
Vojd85 <i7bicak@mail.ru>
2022-08-17 01:22:55 +0300

    домашнее задание 3го семинара

---
+++ HW_Seminar3/HomeTask1/Program.cs
@@ -0,0 +1,48 @@
+﻿// Задача 19
+// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+
+
+// Немного заморочился и сделал так, так как думал что надо создать функцию для любого числа
+
+
+Console.WriteLine("Введите пятизначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+while (number < 10000 || number > 99999) 
+{
+    Console.WriteLine("Неверное число");
+    Console.WriteLine("Введите пятизначное число: ");
+    number = Convert.ToInt32(Console.ReadLine());
+}
+
+int Rev(int Number)
+{
+    string array = Convert.ToString(Number);
+    char[] arrayNumber = array.ToCharArray();
+    Array.Reverse(arrayNumber);
+    string arrayRev = new string (arrayNumber);
+    Number = Convert.ToInt32(arrayRev);
+    return Number;
+}
+int numberRev = Rev(number);
+if (number == numberRev) Console.WriteLine("является палиндромом");
+else Console.WriteLine("не является палиндромом");
+
+// Попробуем через числа:
+
+//  Console.WriteLine("Введите пятизначное число: ");
+//  int number = Convert.ToInt32(Console.ReadLine());
+
+// while (number < 10000 || number > 99999) 
+// {
+//     Console.WriteLine("Неверное число");
+//     Console.WriteLine("Введите пятизначное число: ");
+//     number = Convert.ToInt32(Console.ReadLine());
+// }
+
+// if(number/10000 == number%10 && (number%10000)/1000 == (number%100)/10)
+//     System.Console.WriteLine($"является палиндромом");
+// else System.Console.WriteLine($"не является палиндромом");
---
+++ HW_Seminar3/HomeTask2/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 21
+// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+// A (3,6,8); B (2,1,-7), -> 15.84
+// A (7,-5, 0); B (1,-1,9) -> 11.53
+
+Console.Write("Напишите координату Х точки А: ");
+int xa = Convert.ToInt32(Console.ReadLine());
+Console.Write("Напишите координату Y точки А: ");
+int ya = Convert.ToInt32(Console.ReadLine());
+Console.Write("Напишите координату Z точки А: ");
+int za = Convert.ToInt32(Console.ReadLine());
+Console.Write("Напишите координату Х точки Б: ");
+int xb = Convert.ToInt32(Console.ReadLine());
+Console.Write("Напишите координату Y точки Б: ");
+int yb = Convert.ToInt32(Console.ReadLine());
+Console.Write("Напишите координату Z точки Б: ");
+int zb = Convert.ToInt32(Console.ReadLine());
+
+double result = Math.Sqrt((xb - xa)*(xb - xa) + (yb - ya)*(yb - ya) + (zb - za)*(zb - za));
+Console.WriteLine($"Расстояние между точками {Math.Round(result, 2)}");
