b95ee240059fc3675fda2ff3b1c1b5c1b53a6a82
Vojd85 <i7bicak@mail.ru>
2022-08-30 02:02:41 +0300

    Домашнее задание семинар 6

---
+++ HW_Seminar6/HomeTask1/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+Console.WriteLine("Сколько чисел вы хотите ввести? ");
+int M = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[M];
+int count = 0;
+for(int i = 0; i < M; i++)
+{
+    Console.WriteLine("Введите число:");
+    array[i] = Convert.ToInt32(Console.ReadLine());
+    if(array[i] > 0) count++;
+}
+Console.WriteLine($"Количество чисел больше 0 равняется {count}");
+// Console.WriteLine($"[{String.Join(", ",array)}]" );
---
+++ HW_Seminar6/HomeTask2/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, 
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+System.Console.Write("Введите значение k1 для первой прямой: ");
+double k1 = Convert.ToDouble(Console.ReadLine());
+System.Console.Write("Введите значение b1 для первой прямой: ");
+double b1 = Convert.ToDouble(Console.ReadLine());
+System.Console.Write("Введите значение k2 для второй прямой: ");
+double k2 = Convert.ToDouble(Console.ReadLine());
+System.Console.Write("Введите значение b2 для второй прямой: ");
+double b2 = Convert.ToDouble(Console.ReadLine());
+double x = (b1 - b2) / (k2 - k1);
+double y = k1 * x + b1;
+if(k1 == k2) Console.WriteLine("Прямые не пересекаются");
+else Console.WriteLine($"Точка пересечения [{x};{y}]");
+
+// Не хватило времени продумать, если линии являются одной...
