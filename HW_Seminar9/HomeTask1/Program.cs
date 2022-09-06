c36c8e90c53d0fd8393fa523c2e59fbc70a31fc5
Vojd85 <i7bicak@mail.ru>
2022-09-07 02:48:03 +0300

    В процессе задач 9 семинара

---
+++ HW_Seminar9/HomeTask1/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 64: Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N.
+
+// M = 1; N = 5. -> ""1, 2, 3, 4, 5""
+// M = 4; N = 8. -> ""4, 6, 7, 8""
+
+System.Console.Write("Введите число M: ");
+int numberM = Convert.ToInt32(Console.ReadLine());
+System.Console.Write("Введите число N: ");
+int numberN = Convert.ToInt32(Console.ReadLine());
+
+string PrintNumbersFrom (int start, int end)
+{
+    if (start == end && start < 0) return ("Натуральных чисел нет");
+    if (start == end && start > 0) return start.ToString();
+    if (start > end) 
+    {
+         return (start + ", " + PrintNumbersFrom(start - 1, end));  
+    }  
+    else while (start < 0)
+        {
+        start++;
+        }
+        return (start + ", " + PrintNumbersFrom(start + 1, end));
+}
+
+Console.WriteLine($"Натуральные числа от {numberM} до {numberN}: \"\"{PrintNumbersFrom(numberM, numberN)}\"\"");
